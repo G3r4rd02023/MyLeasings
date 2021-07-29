9cacdadc65b9cfc9af58fe461dfc12de1f2eb8d3
Gerardo Lanza <60949087+glanza007@users.noreply.github.com>
2021-07-29 16:43:17 -0600

    Entities

---
+++ MyLeasings/Data/Entities/Contract.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasings.Data.Entities
+{
+    public class Contract
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Comentarios")]
+        public string Remarks { get; set; }
+
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Fecha Inicio")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Fecha Final")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Esta Activo?")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "Fecha Inicio")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime StartDateLocal => StartDate.ToLocalTime();
+
+        [Display(Name = "Fecha Final")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime EndDateLocal => EndDate.ToLocalTime();
+
+        public Owner Owner { get; set; }
+
+        public Lessee Lessee { get; set; }
+
+        public Property Property { get; set; }
+    }
+}
---
+++ MyLeasings/Data/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasings.Data.Entities
+{
+    public class User:IdentityUser
+    {
+        [Display(Name = "DNI")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Document { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Apellidos")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Dirección")]
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Address { get; set; }
+
+        [Display(Name = "Usuario")]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Display(Name = "Usuario")]
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+    }
+}
